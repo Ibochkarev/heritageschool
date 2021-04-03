49e0f029d1a16daebe645c00ed831f07016c9421
Ibochkarev <ivanx86@gmail.com>
2021-04-03 17:45:36 +0600

    test commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,80 +1,78 @@
-import { Swiper, Navigation, Pagination, Mousewheel } from "swiper"
+import {Swiper, Navigation, Pagination, Mousewheel} from "swiper"
 Swiper.use([Navigation, Pagination, Mousewheel])
 
-document.addEventListener("DOMContentLoaded", () => {
-    const homeSlider = new Swiper(".home-slider .swiper-container", {
+document.addEventListener(`DOMContentLoaded`, () => {
+    const homeSlider = new Swiper(`.home-slider .swiper-container`, {
         loop: false,
         speed: 2400,
         navigation: {
-            nextEl: ".swiper-button-next-main",
-            prevEl: ".swiper-button-prev-main",
+            nextEl: `.swiper-button-next-main`,
+            prevEl: `.swiper-button-prev-main`,
         },
         pagination: {
-            el: ".swiper-pagination-main",
-            type: "bullets",
+            el: `.swiper-pagination-main`,
+            type: `bullets`,
             clickable: true,
         },
     })
 
     // Открытие по кнопке мобильного меню
 
-    const mobileMenu = document.querySelector(".mobile-menu"),
-        menuBtn = document.querySelector(".header__mobile-btn"),
-        menuBtnHamburger = document.querySelector(
-            ".header__mobile-btn .is_open"
-        ),
-        menuBtnClose = document.querySelector(".header__mobile-btn .is_close")
+    const mobileMenu = document.querySelector(`.mobile-menu`)
+    const menuBtn = document.querySelector(`.header__mobile-btn`)
+    const menuBtnHamburger = document.querySelector(
+        `.header__mobile-btn .is_open`
+    )
+    const menuBtnClose = document.querySelector(`.header__mobile-btn .is_close`)
 
-    menuBtn.addEventListener("click", toggleMenu)
+    menuBtn.addEventListener(`click`, toggleMenu)
 
     function toggleMenu() {
-        if (mobileMenu.classList.contains("_is-active")) {
-            mobileMenu.classList.remove("_is-active")
-            menuBtnClose.style.display = "none"
-            menuBtnHamburger.style.display = "block"
+        if (mobileMenu.classList.contains(`_is-active`)) {
+            mobileMenu.classList.remove(`_is-active`)
+            menuBtnClose.style.display = `none`
+            menuBtnHamburger.style.display = `block`
         } else {
-            mobileMenu.classList.add("_is-active")
-            menuBtnHamburger.style.display = "none"
-            menuBtnClose.style.display = "block"
+            mobileMenu.classList.add(`_is-active`)
+            menuBtnHamburger.style.display = `none`
+            menuBtnClose.style.display = `block`
         }
     }
 
     // Открытие под меню
 
     const mobileLevelItem = document.querySelectorAll(
-        ".mobile-menu__sub-item, .is-sub .mobile-menu__link"
+        `.mobile-menu__sub-item, .is-sub .mobile-menu__link`
     )
 
     mobileLevelItem.forEach((el) => {
-        el.addEventListener("click", toggleLevelMenu)
+        el.addEventListener(`click`, toggleLevelMenu)
     })
 
     function toggleLevelMenu(e) {
         e.preventDefault()
-        const toggleTarget = e.target.classList.contains("mobile-menu__link")
+        const toggleTarget = e.target.classList.contains(`mobile-menu__link`)
             ? e.target.parentNode
-            : e.target.classList.contains("mobile-menu__sub-item")
-            ? e.target
-            : e.target.closest(".mobile-menu__sub-item")
-        if (toggleTarget.classList.contains("is-open")) {
-            toggleTarget.classList.remove("is-open")
+            : e.target.classList.contains(`mobile-menu__sub-item`)
+                ? e.target
+                : e.target.closest(`.mobile-menu__sub-item`)
+        if (toggleTarget.classList.contains(`is-open`)) {
+            toggleTarget.classList.remove(`is-open`)
         } else {
-            toggleTarget.classList.add("is-open")
+            toggleTarget.classList.add(`is-open`)
         }
     }
 
     // Вызов формы поиска
-    const BtnSearchClose = document.querySelector(
-            ".search-overlay__close-icon"
-        ),
-        BtnSearchOpen = document.querySelector(".header__search"),
-        searchForm = document.querySelector(".search-overlay")
+    const BtnSearchClose = document.querySelector(`.search-overlay__close-icon`)
+    const BtnSearchOpen = document.querySelector(`.header__search`)
+    const searchForm = document.querySelector(`.search-overlay`)
 
-    BtnSearchOpen.addEventListener("click", () => {
-        searchForm.classList.add("active")
+    BtnSearchOpen.addEventListener(`click`, () => {
+        searchForm.classList.add(`active`)
     })
 
-    BtnSearchClose.addEventListener("click", () => {
-        searchForm.classList.remove("active")
+    BtnSearchClose.addEventListener(`click`, () => {
+        searchForm.classList.remove(`active`)
     })
 })
